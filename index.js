4ad88fa53fbb55ea43393a78cf949e8ad78e687d
AdityRaj99 <adityaraj9983@gmail.com>
2022-04-22 20:19:20 +0530

    error fixed
    error fixed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// express import
+
 const express = require('express')
-// express initialization
+
 const app = express()
 const PORT = 3000
 
 const cors = require("cors");
 const rateLimit = require('express-rate-limit');
-// const rateLimiter = require("./rateLimitter");
 
-// Create the rate limit rule
+
+
 const apiRequestLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
     max: 10, // limit each IP to 10 requests per windowMs
@@ -17,17 +17,16 @@ const apiRequestLimiter = rateLimit({
 })
 
 app.use(cors());
-// Use the limit rule as an application middleware
-app.use(apiRequestLimiter)
-// app.use(rateLimiter);
 
-// generic GET route that we will use for the tests
+app.use(apiRequestLimiter)
+
+
 app.get('/', function (req, res) {
   return res.send('Hello World')
 })
 
 
-// server initialization 
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
